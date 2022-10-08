c17b506632354e374c2a34fdf1eee49abbc0e1c8
richard <richard.li@biposervice.com>
2022-10-08 16:33:27 +0800

    reformat

--- ruoyi-auth/src/main/java/com/ruoyi/auth/form/LoginBody.java
+++ ruoyi-auth/src/main/java/com/ruoyi/auth/form/LoginBody.java
@@ -1,39 +1,17 @@
 package com.ruoyi.auth.form;
 
+import lombok.Data;
+
 /**
  * 用户登录对象
- * 
+ *
  * @author ruoyi
  */
-public class LoginBody
-{
-    /**
-     * 用户名
-     */
-    private String username;
+@Data
+public class LoginBody {
 
-    /**
-     * 用户密码
-     */
-    private String password;
+    private String username;//用户名
 
-    public String getUsername()
-    {
-        return username;
-    }
+    private String password;//用户密码
 
-    public void setUsername(String username)
-    {
-        this.username = username;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
-
-    public void setPassword(String password)
-    {
-        this.password = password;
-    }
 }
